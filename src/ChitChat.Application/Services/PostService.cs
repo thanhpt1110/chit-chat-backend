5774c634adb0aaf4bda04dddb2ce7cf77dda8863
Thanh Phan Tuan <69446444+thanhpt1110@users.noreply.github.com>
2024-11-30 11:37:28 +0700

    fix: Get all post filter by ID (#38)
    Co-authored-by: Nguyễn Phúc Bình <96096830+leesoonduck3009@users.noreply.github.com>

--- src/ChitChat.Application/Services/PostService.cs
+++ src/ChitChat.Application/Services/PostService.cs
@@ -62,11 +62,11 @@ namespace ChitChat.Application.Services
             {
                 throw new NotFoundException(ValidationTexts.NotFound.Format("User", userId));
             }
-
+            filter = filter.And(p => p.UserId == userId);
             var paginationResponse = await _postRepository.GetAllAsync(filter, p => p.OrderByDescending(p => p.CreatedOn)
                                                             , query.PageIndex
                                                             , query.PageSize,
-                                                            p => p.IgnoreAutoIncludes().Include(c => c.PostMedias)
+                                                            p => p.IgnoreAutoIncludes().Include(c => c.PostMedias).Include(c => c.User)
                                                             );
             foreach (var post in paginationResponse.Items)
             {
