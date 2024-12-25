20e6d435775fb008d2d1bbe75ba5649ad0a27671
PNB309 <nguyen.phucbinh445@gmail.com>
2024-12-25 18:01:54 +0700

    fix

--- src/ChitChat.Application/Services/FollowService.cs
+++ src/ChitChat.Application/Services/FollowService.cs
@@ -6,6 +6,7 @@ using ChitChat.Application.Models.Dtos.Notification;
 using ChitChat.Application.Models.Dtos.User;
 using ChitChat.Application.Models.Dtos.User.Follow;
 using ChitChat.Application.Services.Interface;
+using ChitChat.DataAccess.Repositories.Interface;
 using ChitChat.DataAccess.Repositories.Interrface;
 using ChitChat.Domain.Common;
 using ChitChat.Domain.Entities.UserEntities;
