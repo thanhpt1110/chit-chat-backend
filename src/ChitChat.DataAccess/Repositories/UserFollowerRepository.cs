9abb53bc17b5e29c17a1ac05eb415595bcc76b1f
Nguyễn Phúc Bình <96096830+leesoonduck3009@users.noreply.github.com>
2024-12-28 15:58:41 +0700

    Feature/CCT-42 (#55)
    ticket: https://uit-project.atlassian.net/browse/CCT-42
    
    ---------
    
    Co-authored-by: Tuan-Thanh <thanhphantuan1110@gmail.com>

--- src/ChitChat.DataAccess/Repositories/UserFollowerRepository.cs
+++ src/ChitChat.DataAccess/Repositories/UserFollowerRepository.cs
@@ -1,6 +1,7 @@
 using ChitChat.DataAccess.Data;
 using ChitChat.DataAccess.Repositories.Interface;
 using ChitChat.Domain.Entities.UserEntities;
+using ChitChat.Domain.Identity;
 
 namespace ChitChat.DataAccess.Repositories
 {
@@ -10,7 +11,7 @@ namespace ChitChat.DataAccess.Repositories
         {
         }
 
-        public async Task<List<UserFollower>> GetRecommendedUsersAsync(string currentUserId)
+        public async Task<List<UserApplication>> GetRecommendedUsersAsync(string currentUserId, int pageSize)
         {
             var followedUsers = await this.Context.UserFollowers
                 .Where(uf => uf.UserId == currentUserId)
@@ -20,10 +21,20 @@ namespace ChitChat.DataAccess.Repositories
                 .Where(uf => followedUsers.Contains(uf.UserId) // Người bạn của tôi theo dõi
                              && uf.FollowerId != currentUserId // Không phải chính tôi
                              && !followedUsers.Contains(uf.FollowerId))
-                .Include(p => p.User)// Tôi chưa theo dõi họ
+                .Include(p => p.User)
+                .Select(p => p.User)// Tôi chưa theo dõi họ
                 .Distinct() // Loại bỏ trùng lặp
                 .ToListAsync();
-            return recommendedUsers;
+            var otherFollow = await Context.UserApplications
+                .Where(p => p.Id != currentUserId//  Tôi chưa theo dõi họ
+                             && !followedUsers.Contains(p.Id))// Họ chưa theo dõi tôi
+                .ToListAsync();
+            var allUsers = recommendedUsers
+            .Concat(otherFollow) // Gộp với otherFollow
+            .ToList();
+            // Random hóa danh sách
+            var randomUsers = allUsers.OrderBy(u => Guid.NewGuid()).ToList();
+            return randomUsers.Take(pageSize).ToList();
         }
     }
 }
